047305674cf3689639178cdac15b6642c81fad5d
arsalanmuntaha1 <arsalanmuntaha1@gmail.com>
2024-12-27 21:44:46 +0500

    WIP

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,9 @@
+import Header from '@/components/header';
+
 export default function About() {
-    return <div className="text-white">About</div>;
+    return (
+        <div className="text-">
+            <Header pageName="About Us" pageRoute="About" />
+        </div>
+    );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -1,35 +1,64 @@
-import { inter } from '@/app/fonts/index';
+import { helvetica, inter } from '@/app/fonts/index';
 import Link from 'next/link';
 import React from 'react';
 import { FiSearch } from 'react-icons/fi';
 import { LiaShoppingBagSolid } from 'react-icons/lia';
-import { PiUser } from 'react-icons/pi';
+import { PiCaretRightBold, PiUser } from 'react-icons/pi';
 
-function Header() {
+type propsType = {
+    pageName: string;
+    pageRoute: string;
+};
+
+function Header({ pageName, pageRoute }: propsType) {
     return (
-        <div className={`${inter.className} text-textColor bg-bg`}>
-            <div>
-                <h5 className="font-bold text-2xl">
-                    Food<span className="text-primary3">tuck</span>
-                </h5>
+        <div
+            className={`${inter.className} bg-[url("/assets/menubg.svg")] bg-no-repeat bg-cover text-textColor `}
+        >
+            <div className='flex justify-around py-7 bg-bg'>
+                <div>
+                    <h5 className="font-bold text-2xl">
+                        Food<span className="text-primary3">tuck</span>
+                    </h5>
+                </div>
+                <div>
+                    <ul className="flex gap-8">
+                        <li className="hover:text-primary3">
+                            <Link href={'/'}>Home</Link>
+                        </li>
+                        <li className="hover:text-primary3">
+                            <Link href={'/menu'}>Menu</Link>
+                        </li>
+                        <li className="hover:text-primary3">
+                            <Link href={'/blog'}>Blog</Link>
+                        </li>
+                        <li className="hover:text-primary3">
+                            <Link href={'/pages'}>Pages</Link>
+                        </li>
+                        <li className="hover:text-primary3">
+                            <Link href={'/about'}>About</Link>
+                        </li>
+                        <li className="hover:text-primary3">
+                            <Link href={'/shop'}>Shop</Link>
+                        </li>
+                        <li className="hover:text-primary3">
+                            <Link href={'/contact'}>Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="flex gap-4">
+                    <FiSearch size={24} className="self-center " />
+                    <PiUser size={24} className="self-center " />
+                    <LiaShoppingBagSolid size={24} className="self-center " />
+                </div>
             </div>
-            <div className="flex gap-8">
-                <ul>
-                    <li className="hover:text-primary3">Home</li>
-                    <li>
-                        <Link href={'/menu'}>Menu</Link>
-                    </li>
-                    <li>Blog</li>
-                    <li>Pages</li>
-                    <li>About</li>
-                    <li>Shop</li>
-                    <li>Contact</li>
-                </ul>
-            </div>
-            <div className="flex gap-4">
-                <FiSearch color="#FFFFFF" size={24} />
-                <PiUser />
-                <LiaShoppingBagSolid size={24} color="#FFFFFF" className="self-center " />
+            <div className='py-28 place-items-center'>
+                <h2 className={`${helvetica.className} font-bold text-5xl `}>{pageName}</h2>
+                <div className="flex gap-2 mt-5">
+                    <p className="text-xl">Home</p>
+                    <PiCaretRightBold className="self-center" />
+                    <p className="text-xl text-primary3">{pageRoute}</p>
+                </div>
             </div>
         </div>
     );
